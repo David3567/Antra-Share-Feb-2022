ad87dfa16efe334f0a1b503a0e7d84df0fee8856
jhxiaoo <junhui.xiaoo@gmail.com>
2022-03-04 03:46:08 -0500

    update

--- buttoncard/src/app/carditem/carditem.component.ts
+++ buttoncard/src/app/carditem/carditem.component.ts
@@ -9,12 +9,38 @@ import { Card } from '../interfaces/card.model';
 export class CarditemComponent implements OnInit {
   @Input() carditem!: Card;
   @Output() cardEmiter = new EventEmitter();
+  Style: String[] = ['blue', 'black', 'red', 'green'];
 
   constructor() {}
 
   ngOnInit(): void {}
 
-  changeColor() {
-    console.log('Got it');
+  clickChangeTitleColor(event: Event) {
+    const test = document.querySelector('.title') as HTMLElement;
+    test.style.color = window
+      .getComputedStyle(event.target as HTMLButtonElement, null)
+      .getPropertyValue('background-color');
+  }
+
+  focusChangeLiColor(event: Event) {
+    const test = (event.target as HTMLButtonElement).parentElement;
+    let border_style =
+      '2px solid ' +
+      window
+        .getComputedStyle(event.target as HTMLButtonElement, null)
+        .getPropertyValue('background-color');
+    test!.style.border = border_style;
+    let box_shadow_style =
+      '5px 5px 5px ' +
+      window
+        .getComputedStyle(event.target as HTMLButtonElement, null)
+        .getPropertyValue('background-color');
+    test!.style.boxShadow = box_shadow_style;
+  }
+
+  focusoutChangeLiColor(event: Event) {
+    const test = (event.target as HTMLButtonElement).parentElement;
+    test!.style.border = 'none';
+    test!.style.boxShadow = 'none';
   }
 }
--- buttoncard/src/app/cardlist/cardlist.component.ts
+++ buttoncard/src/app/cardlist/cardlist.component.ts
@@ -10,25 +10,26 @@ export class CardlistComponent implements OnInit {
   cardlist: Card[] = [
     {
       id: 1,
-      title: 'number 1',
-      body: 'Hello World~',
+      title: 'Li No.1',
+      body: 'Hello World+',
     },
     {
       id: 2,
-      title: 'number 2',
-      body: 'Hello World.',
+      title: 'Li No.2',
+      body: 'Hello World-',
     },
     {
       id: 3,
-      title: 'number 3',
-      body: 'Hello World!',
+      title: 'Li No.3',
+      body: 'Hello World*',
     },
     {
       id: 4,
-      title: 'number 4',
-      body: 'Hello World_',
+      title: 'Li No.4',
+      body: 'Hello World/',
     },
   ];
+
   constructor() {}
 
   ngOnInit(): void {}
