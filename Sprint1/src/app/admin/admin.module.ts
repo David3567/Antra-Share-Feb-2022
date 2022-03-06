83b340c7f08705e2a30b751b92f539e1bd0b503f
jhxiaoo <junhui.xiaoo@gmail.com>
2022-03-06 05:49:49 -0500

    update

--- Sprint1/src/app/admin/admin.module.ts
+++ Sprint1/src/app/admin/admin.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { AdminComponent } from './admin.component';
 import { UserinfoComponent } from './userinfo/userinfo.component';
 import { UserlistComponent } from './userlist/userlist.component';
-import { AdminComponent } from './admin.component';
 
 @NgModule({
-  declarations: [UserinfoComponent, UserlistComponent, AdminComponent],
+  declarations: [AdminComponent, UserinfoComponent, UserlistComponent],
   imports: [CommonModule],
 })
 export class AdminModule {}
--- Sprint1/src/app/app-routing.module.ts
+++ Sprint1/src/app/app-routing.module.ts
@@ -1,10 +1,37 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminComponent } from './admin/admin.component';
+import { LoginComponent } from './login/login.component';
+import { NewsfeedComponent } from './newsfeed/newsfeed.component';
+import { ProfileComponent } from './profile/profile.component';
+import { SettingComponent } from './setting/setting.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // { path: '', redirectTo: '/login', pathMatch: 'full' },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: 'admin',
+    component: AdminComponent,
+  },
+  {
+    path: 'newsfeed',
+    component: NewsfeedComponent,
+  },
+  {
+    path: 'profile',
+    component: ProfileComponent,
+  },
+  {
+    path: 'setting',
+    component: SettingComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
