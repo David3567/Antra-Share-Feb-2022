e0a33808539e47f123d6424cd594ee46f7d2237a
kuku20 <lluu3790@gmail.com>
2022-03-06 13:43:41 -0600

    add userlist

--- antraShares/src/app/admin/admin.module.ts
+++ antraShares/src/app/admin/admin.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AdminComponent } from './admin.component';
+import { UserinfoComponent } from './userinfo/userinfo.component';
 
 
 
 @NgModule({
-  declarations: [AdminComponent],
+  declarations: [AdminComponent, UserinfoComponent],
   imports: [
     CommonModule
   ],
