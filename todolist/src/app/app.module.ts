44793653ed0d7aac267d723ebc0c66316a82a37a
baselHassan <baselhassan8@gmail.com>
2022-03-07 07:34:33 -0600

    project init

---
+++ todolist/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TodolistComponent } from './todolist/todolist.component';
+import { TodoitemComponent } from './todoitem/todoitem.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TodolistComponent,
+    TodoitemComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ todolist/src/app/todoitem/todoitem.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-todoitem',
+  templateUrl: './todoitem.component.html',
+  styleUrls: ['./todoitem.component.scss']
+})
+export class TodoitemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
