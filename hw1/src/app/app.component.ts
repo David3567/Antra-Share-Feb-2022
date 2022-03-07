b57cfdd80e6b50581c20d0b09443101a9b25e7c8
adib thaqif <mtaufek@ucsd.edu>
2022-03-06 17:39:59 -0800

    Implemented color changing title

--- hw1/src/app/app.component.ts
+++ hw1/src/app/app.component.ts
@@ -7,4 +7,50 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'hw1';
+
+  titleColor = 'red';
+
+  newsList: any = [
+    {
+      id: 0,
+      title: "Lorem ipsum, dolor sit amet consectetur adipisicing elit.",
+      location: "MADISON. WI",
+      content: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cumque dicta ab nam consequatur accusamus asperiores officiis qui aliquid quod error non a ipsum porro voluptatibus, hic natus placeat quia amet! Impedit dolor eum quasi velit? Explicabo, suscipit eaque quod eos ipsam, excepturi provident animi iste fugiat, unde sapiente. Id, nemo!",
+      color: "blue",
+      hasBoxShadow: false
+    },
+    {
+      id: 1,
+      title: "Lorem ipsum, dolor sit amet consectetur adipisicing elit.",
+      location: "MADISON. WI",
+      content: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cumque dicta ab nam consequatur accusamus asperiores officiis qui aliquid quod error non a ipsum porro voluptatibus, hic natus placeat quia amet! Impedit dolor eum quasi velit? Explicabo, suscipit eaque quod eos ipsam, excepturi provident animi iste fugiat, unde sapiente. Id, nemo!",
+      color: "black",
+      hasBoxShadow: false
+    },
+    {
+      id: 2,
+      title: "Lorem ipsum, dolor sit amet consectetur adipisicing elit.",
+      location: "MADISON. WI",
+      content: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cumque dicta ab nam consequatur accusamus asperiores officiis qui aliquid quod error non a ipsum porro voluptatibus, hic natus placeat quia amet! Impedit dolor eum quasi velit? Explicabo, suscipit eaque quod eos ipsam, excepturi provident animi iste fugiat, unde sapiente. Id, nemo!",
+      color: "red",
+      hasBoxShadow: false
+    },
+    {
+      id: 3,
+      title: "Lorem ipsum, dolor sit amet consectetur adipisicing elit.",
+      location: "MADISON. WI",
+      content: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cumque dicta ab nam consequatur accusamus asperiores officiis qui aliquid quod error non a ipsum porro voluptatibus, hic natus placeat quia amet! Impedit dolor eum quasi velit? Explicabo, suscipit eaque quod eos ipsam, excepturi provident animi iste fugiat, unde sapiente. Id, nemo!",
+      color: "green",
+      hasBoxShadow: false
+    }
+  ];
+
+  setColor(index: number){
+    this.titleColor = this.newsList[index].color;
+    this.newsList = this.newsList.map((news: { id: number; hasBoxShadow: boolean; }) => {
+      if(news.id === index) news.hasBoxShadow = !news.hasBoxShadow;
+      else news.hasBoxShadow = false;
+      return news;
+    })
+  }
 }
--- hw1/src/app/card/card.component.ts
+++ hw1/src/app/card/card.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
-  selector: 'app-card',
+  selector: 'app-news-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.css']
 })
 export class CardComponent implements OnInit {
 
+  @Input()
+  news!: { id: number; location: string; content: string; color: string; hasBoxShadow: boolean; };
+  @Output() buttonClicked = new EventEmitter();
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  setColor(index: number) {
+    this.buttonClicked.emit(index);
   }
 
 }
