3865333a095371791a6b5680117b9e04c9261fb5
baselHassan <baselhassan8@gmail.com>
2022-03-07 12:31:11 -0600

    project init and structure complete

---
+++ News/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+import { AdminModule } from './admin/admin.module';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginModule } from './login/login.module';
+import { NewsFeedModule } from './news-feed/news-feed.module';
+import { ProfileModule } from './profile/profile.module';
+import { SettingsModule } from './settings/settings.module';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    AdminModule,
+    LoginModule,
+    NewsFeedModule,
+    ProfileModule,
+    SettingsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
