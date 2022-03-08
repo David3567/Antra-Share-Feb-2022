2c39d07a797dca729d1500b9c249799bc754dc28
show3567 <ydong011@fiu.edu>
2022-03-08 15:54:39 -0600

    update

--- Angular/observable/src/app/app.component.ts
+++ Angular/observable/src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { from, fromEvent, Observable, of } from 'rxjs';
+import { filter, map, tap } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,7 +9,37 @@ import { Observable } from 'rxjs';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
-  title = 'observable';
+  @ViewChild('inputbox', { static: true }) inputbox!: ElementRef;
 
-  ngOnInit(): void {}
+  title = 'observable';
+  observable$ = from([3, 4, 5]);
+  val = '';
+
+  getUsers$ = this.http.get('https://jsonplaceholder.typicode.com/users').pipe(
+    map((users: any) => {
+      return users.map((user: any) => {
+        return {
+          ...user,
+          username: `${user.username} - (${user.name})`,
+        };
+      });
+    }),
+    tap((users) => {
+      console.log(users);
+    })
+  );
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    fromEvent(this.inputbox.nativeElement, 'keyup')
+      .pipe(filter((e: any) => e.code === 'Enter'))
+      .subscribe((e) => {
+        console.log(this.val);
+      });
+  }
+
+  getUsers() {
+    this.getUsers$.subscribe();
+  }
 }
--- Angular/observable/src/app/app.module.ts
+++ Angular/observable/src/app/app.module.ts
@@ -1,16 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, FormsModule, HttpClientModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
