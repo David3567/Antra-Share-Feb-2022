269851e1d9453c2315c9f31232c39485bb68629f
baselHassan <baselhassan8@gmail.com>
2022-03-08 14:21:03 -0600

    admin page styled

--- News/src/app/admin/admin.module.ts
+++ News/src/app/admin/admin.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AdminComponent } from './admin.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { UserDetailsComponent } from './user-details/user-details.component';
 
 
 
 @NgModule({
   declarations: [
-    AdminComponent
+    AdminComponent,
+    UserListComponent,
+    UserDetailsComponent
   ],
   imports: [
     CommonModule
