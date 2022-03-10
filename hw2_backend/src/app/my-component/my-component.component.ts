cefbae261981a0d8d6b5e5d916d4b8e411a77193
kpatel7c8 <kpatel7c8@gmail.com>
2022-03-10 11:08:49 -0600

    add interface

--- hw2_backend/src/app/my-component/my-component.component.ts
+++ hw2_backend/src/app/my-component/my-component.component.ts
@@ -1,6 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from "@angular/common/http";
 
+interface DataModel {
+  _id: string;
+  publisherName: string;
+  //content: object;
+}
+
+
 @Component({
   selector: 'app-my-component',
   templateUrl: './my-component.component.html',
@@ -11,14 +18,16 @@ export class MyComponentComponent implements OnInit {
   //url = 'https://jsonplaceholder.typicode.com/users'
   url = 'http://localhost:4231/api/news'
 
+
   constructor(private http: HttpClient) {
+    this.http.get(this.url).subscribe((result: any)=> {
+      console.log(result);
+      //this.data = result;
+      console.log(result[0].publisherName);
+    })
    }
 
   ngOnInit() {
-    this.http.get(this.url).subscribe((result)=> {
-      console.log(result)
-    })
   }
-
 }
 
