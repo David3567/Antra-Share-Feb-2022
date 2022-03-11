9a3c5460ef627a41d60e27b676789afc3473dba5
kpatel7c8 <kpatel7c8@gmail.com>
2022-03-11 16:31:49 -0600

    create 5 modules

--- project1/src/app/admin/admin.module.ts
+++ project1/src/app/admin/admin.module.ts
@@ -1,19 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { AdminPageComponent } from './admin-page/admin-page.component';
-import { UserInfoPanelComponent } from './userInfoPanel/userInfoPanel.component';
-import {MatIconModule} from '@angular/material/icon';
-import {MatTableModule} from '@angular/material/table';
 
 @NgModule({
-  declarations: [
-    AdminPageComponent,
-    UserInfoPanelComponent
-  ],
+  declarations: [],
   imports: [
-    CommonModule,
-    MatIconModule,
-    MatTableModule
+    CommonModule
   ]
 })
 export class AdminModule {
