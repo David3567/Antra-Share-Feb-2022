ba39ca5219afc1afaada2395d01bb7a4ab85859b
Ardit <ardit.hoxha@gmail.com>
2022-03-12 18:55:39 -0500

    starting task for liked list

--- TeamProject/src/app/modules/news-feed/news-feed.module.ts
+++ TeamProject/src/app/modules/news-feed/news-feed.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NewsfeedPageComponent } from './newsfeed-page/newsfeed-page.component';
+import { PostnewsComponent } from './newsfeed-page/postnews/postnews.component';
+import { StoryComponent } from './newsfeed-page/story/story.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    NewsfeedPageComponent,
+    PostnewsComponent,
+    StoryComponent
+  ],
   imports: [
     CommonModule
   ]
