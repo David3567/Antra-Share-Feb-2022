5cd37a1301484526f703683d4667e68b9967bfb0
SuchenShi <Suchen.Shi.TH@dartmouth.edu>
2022-03-11 19:24:50 -0500

    updated setting page

--- TeamProject/src/app/modules/login/login.module.ts
+++ TeamProject/src/app/modules/login/login.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-
-
 @NgModule({
-  declarations: [],
+  declarations: [
+  
+  ],
   imports: [
     CommonModule
-  ]
+  ],
+
 })
 export class LoginModule { }
