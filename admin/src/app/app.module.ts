7d07916c04981e9c941176f5b593b8d7806a8506
jie-ying <99215630+jie-ying@users.noreply.github.com>
2022-03-13 13:58:13 -0500

    update

--- admin/src/app/app.module.ts
+++ admin/src/app/app.module.ts
@@ -2,7 +2,9 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
-import { MatSliderModule } from '@angular/material/slider';
+import {MatButtonModule} from '@angular/material/button';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatIconModule} from '@angular/material/icon';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -23,7 +25,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     HttpClientModule,
     FormsModule,
     BrowserAnimationsModule,
-    MatSliderModule
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule
   ],
   providers: [UserlistService],
   bootstrap: [AppComponent]
