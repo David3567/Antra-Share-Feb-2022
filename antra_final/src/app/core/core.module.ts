aa84ab5c0c003cbebb6d1e3daead3480e866dc05
lukeyzhong <lukeyzhong@gmail.com>
2022-03-12 18:19:11 -0600

    Co-authored-by: Kamil Syed <kamilrsyed@users.noreply.github.com> Co-authored-by: tawenna <tawenna@users.noreply.github.com>

--- antra_final/src/app/core/core.module.ts
+++ antra_final/src/app/core/core.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
-import { FooterComponent } from './footer/footer.component';
 import { SharedModule } from '../shared/shared.module';
 
 
@@ -9,7 +8,6 @@ import { SharedModule } from '../shared/shared.module';
 @NgModule({
   declarations: [
     HeaderComponent,
-    FooterComponent
   ],
   imports: [
     CommonModule,
@@ -17,7 +15,6 @@ import { SharedModule } from '../shared/shared.module';
   ],
   exports: [
     HeaderComponent,
-    FooterComponent
   ]
 })
 export class CoreModule { }
