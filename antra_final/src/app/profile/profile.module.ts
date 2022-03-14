a4d4cadf3e80957babb811b74268cf1c76d74347
Kamil Syed <krsyed1@gmail.com>
2022-03-14 10:26:43 -0400

    GUI Log in and Profile

--- antra_final/src/app/profile/profile.module.ts
+++ antra_final/src/app/profile/profile.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedModule } from '../shared/shared.module';
+import { ProfileComponent } from './profile.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ProfileComponent
+  ],
   imports: [
     CommonModule,
     SharedModule,
+  ],
+  exports: [
+    ProfileComponent
   ]
 })
 export class ProfileModule { }
