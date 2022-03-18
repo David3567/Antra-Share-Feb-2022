966ea26d5ebf36821748c8d41c1343b6ebbcd0ed
Ardit <ardit.hoxha@gmail.com>
2022-03-17 21:30:17 -0400

    used Reactive Forms to set up login. Also, adding css to keep the form in the viewport if it gets larger than it

--- TeamProject/src/app/modules/login/login-page/login-page.component.ts
+++ TeamProject/src/app/modules/login/login-page/login-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login-page',
@@ -7,9 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPageComponent implements OnInit {
 
-  constructor() { }
+  loginForm: FormGroup;
+
+  constructor(private build: FormBuilder) { }
 
   ngOnInit(): void {
+    this.loginForm = this.build.group({
+      username: ['', [Validators.minLength(5), Validators.maxLength(12), Validators.required]],
+      password: ["",[Validators.minLength(5),Validators.pattern("^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[#?!@$%^&*-]).{5,}$"), Validators.required]]
+    });
+  }
+
+  get username() {
+    return this.loginForm.get('username');
+  }
+
+  get password() {
+    return this.loginForm.get('password');
+  }
+
+  onSubmit(login: FormGroup) {
+    console.log(login.value);
   }
 
 }
