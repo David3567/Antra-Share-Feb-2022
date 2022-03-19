6a9eb05b55f11f2fb1aae068f469be806235dd5f
jqj5405 <35941304+jqj5405@users.noreply.github.com>
2022-03-19 13:43:43 -0400

    Create reactive profile

--- TeamProject/src/app/modules/profile/profile.module.ts
+++ TeamProject/src/app/modules/profile/profile.module.ts
@@ -1,14 +1,27 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatRadioModule } from '@angular/material/radio';
 
 import { ProfilePageComponent } from './profile-page/profile-page.component';
 
 @NgModule({
   declarations: [ProfilePageComponent],
-  imports: [CommonModule, MatCardModule, MatButtonModule, MatIconModule],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatCardModule,
+    MatButtonModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatRadioModule,
+  ],
   exports: [ProfilePageComponent],
 })
 export class ProfileModule {}
