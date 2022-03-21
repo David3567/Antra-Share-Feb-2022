ddb350bf4478b0fa897317d5627e0c871f139f62
SuchenShi <Suchen.Shi.TH@dartmouth.edu>
2022-03-21 14:27:10 -0400

    Paginator

--- TeamProject/src/app/modules/news-feed/newsfeed-page/story-card/comment-dialog/comment-dialog.component.ts
+++ TeamProject/src/app/modules/news-feed/newsfeed-page/story-card/comment-dialog/comment-dialog.component.ts
@@ -7,11 +7,48 @@ import { MAT_DIALOG_DATA } from '@angular/material/dialog';
   styleUrls: ['./comment-dialog.component.css'],
 })
 export class CommentDialogComponent implements OnInit {
-  commentList: Comment;
+  commentList: string[];
+  pageList: string[];
+  currentPage: number = 1;
+  pageSize: number;
+  head: number = 0;
+  tail: number = 6;
+  pageCapacity: number = this.tail - this.head;
+  prevBtnDisabled: boolean = true;
+  nextBtnDisabled: boolean = true;
 
-  constructor(@Inject(MAT_DIALOG_DATA) public data: { comment: Comment }) {}
+  constructor(@Inject(MAT_DIALOG_DATA) public data: { comment: string[] }) {}
 
   ngOnInit(): void {
     this.commentList = this.data.comment;
+    this.pageList = this.commentList.slice(this.head, this.tail);
+    this.pageSize = Math.ceil(this.commentList.length / this.pageCapacity);
+    this.pageSize > 1 ? (this.nextBtnDisabled = false) : true;
+  }
+
+  onPrev() {
+    this.head -= this.pageCapacity;
+    this.tail -= this.pageCapacity;
+    this.currentPage--;
+    this.pageList = this.commentList.slice(this.head, this.tail);
+    if (this.head === 0) {
+      this.prevBtnDisabled = true;
+      this.nextBtnDisabled = false;
+    } else {
+      this.nextBtnDisabled = false;
+    }
+  }
+
+  onNext() {
+    this.head += this.pageCapacity;
+    this.tail += this.pageCapacity;
+    this.currentPage++;
+    this.pageList = this.commentList.slice(this.head, this.tail);
+    if (this.tail === this.pageSize * this.pageCapacity) {
+      this.nextBtnDisabled = true;
+      this.prevBtnDisabled = false;
+    } else {
+      this.prevBtnDisabled = false;
+    }
   }
 }
