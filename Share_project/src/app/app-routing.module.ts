d33a69b314abb96826490eb1a4f1a3a8d766bbb4
ZT <zt@ZTs-MacBook-Pro.local>
2022-03-21 13:38:02 -0700

    some updates

--- Share_project/src/app/app-routing.module.ts
+++ Share_project/src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { FavoriteListComponent } from './news-feed/favorite-list/favorite-list.component';
+import { NewsFeedComponent } from './news-feed/news-feed.component';
+import { ProfileComponent } from './profile/profile.component';
+import { SettingsComponent } from './settings/settings.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'My Profile', component: ProfileComponent},
+  {path: 'News Feed', component: NewsFeedComponent},
+  {path: 'Settings', component: SettingsComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'favorite', component: FavoriteListComponent},
+
+  {path: '', redirectTo: 'login', pathMatch:'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Share_project/src/app/login/login.component.ts
+++ Share_project/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginInfo, admin, user } from './models/loginInfo';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  info = new LoginInfo();
+  check = true;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(loginForm: any) {
+
+  }
+
+  checkLoginInfo(name: string, pass: string) {
+    if(admin.getName() === name && admin.getPass() === pass) {
+      console.log("admin");
+    }
+    else if (user.getName() === name && user.getPass() === pass) {
+      console.log("user");
+    }
+  }
+
+  checkBox(check:boolean) {
+    console.log(check)
+    this.check = !check;
   }
 
 }
