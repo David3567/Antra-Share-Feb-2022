cc56b81a8a41bcd9723cdf81ecc72b90b333b1cd
Hanzhang Bai <johnkramerbhz@gmail.com>
2022-03-21 03:34:19 -0400

    Login with validator

--- antraShares/src/app/login/login.component.ts
+++ antraShares/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  form!: FormGroup;
+  constructor(private fb: FormBuilder) { }
 
-  constructor() { }
+  ngOnInit() {
+    this.form = this.fb.group({
+      username: new FormControl('', [Validators.minLength(5), Validators.required, Validators.maxLength(12)]),
+      password: new FormControl('', [Validators.minLength(5), Validators.required, this.checkUppercaseLetter(), this.checkSpecialLetter()]),
+    }, {});
+  }
 
-  ngOnInit(): void {
+
+
+  onSubmit() {
+    console.log(this.form.value);
+  }
+  get username() {
+    return this.form.get('username');
+  }
+  get password() {
+    return this.form.get('password');
+  }
+  checkSpecialLetter(): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      const specialLeter = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]+/;
+      if (!specialLeter.test(control.value)) {
+        return { specialLeter: true }
+      }
+      else return null;
+    };
+  }
+  checkUppercaseLetter(): ValidatorFn {
+    return (control: AbstractControl): ValidationErrors | null => {
+      const uppercaseLetter = /[A-Z]+/;
+      if (!uppercaseLetter.test(control.value)) {
+        return { uppercaseLetter: true }
+      }
+      else return null;
+
+    };
   }
 
 }
