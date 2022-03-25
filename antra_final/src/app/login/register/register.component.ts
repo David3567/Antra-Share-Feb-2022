dc2da5880ce2d8dcd82cc5dde6c17bb4b9c4f96c
Kamil Syed <krsyed1@gmail.com>
2022-03-25 15:48:19 -0400

    some progress

--- antra_final/src/app/login/register/register.component.ts
+++ antra_final/src/app/login/register/register.component.ts
@@ -1,5 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, Validators, FormBuilder, ValidationErrors } from '@angular/forms';
+import { FormGroup, Validators, FormBuilder, ValidationErrors, AbstractControl, AsyncValidatorFn } from '@angular/forms';
+import { RegisterService } from 'src/app/core/register.service';
+import {
+  debounceTime,
+  map,
+  tap,
+  switchMap,
+  first,
+  take,
+  distinctUntilChanged,
+  mapTo,
+  catchError,
+} from 'rxjs/operators';
+import { Observable, of, timer } from 'rxjs';
 
 @Component({
   selector: 'app-register',
@@ -8,19 +21,21 @@ import { FormGroup, Validators, FormBuilder, ValidationErrors } from '@angular/f
 })
 export class RegisterComponent implements OnInit {
   form: FormGroup = this.fb.group({
-    username: ['', [Validators.required, 
-      Validators.minLength(5), 
-      Validators.maxLength(12), 
-    ]],
+    username: ['', [Validators.required,
+    Validators.minLength(5),
+    Validators.maxLength(12),
+    ],
+      [this.myAsyncValidator()],
+    ],
     email: ['', [Validators.required,
-       Validators.email]],
+    Validators.email],],
     passwd: ['', [Validators.required,
-      Validators.pattern("^(?=.*[A-Z])(?=.*[$@$!%*?&~]).{5,}")]],
+    Validators.pattern("^(?=.*[A-Z])(?=.*[$@$!%*?&~]).{5,}")]],
     confirm: ['', [Validators.required,]],
   },
-  {
-    validators: this.matchPassword,
-  }
+    {
+      validators: this.matchPassword,
+    }
   );
 
   get username() {
@@ -39,9 +54,10 @@ export class RegisterComponent implements OnInit {
     return this.form.get('confirm');
   }
 
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder,
+    private registerService: RegisterService) { }
 
-  ngOnInit(): void {}
+  ngOnInit(): void { }
 
   onSubmit() {
     console.log(this.form.value);
@@ -54,4 +70,32 @@ export class RegisterComponent implements OnInit {
     return passwd !== confirm ? { notMatch: true } : null;
   }
 
+  private myAsyncValidator(): AsyncValidatorFn {
+    return (control: AbstractControl): Observable<ValidationErrors | null> => {
+      return timer(5000).pipe(
+        switchMap((val: any) => {
+          console.log(1111111);
+          return this.registerService.getUsername(control.value);
+        }),
+        tap(console.log),
+        mapTo({ hasuser: true }),
+        catchError((err) => of(null))
+      );
+    };
+  }
+
+}
+
+export function myAsyncValidator(
+  registerService: RegisterService
+): AsyncValidatorFn {
+  return (control: AbstractControl): Observable<ValidationErrors | null> => {
+    console.log(control);
+    return registerService.getUsername(control.value).pipe(
+      map((result: any) => {
+        console.log(result);
+        return result ? null : { invalidAsync: true };
+      })
+    );
+  };
 }
