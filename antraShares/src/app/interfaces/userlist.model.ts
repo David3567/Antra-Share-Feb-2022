c7bd973d0a5d3c091a7f16123ea2f9bf823f290e
kuku20 <lluu3790@gmail.com>
2022-03-26 20:07:47 -0500

    login

--- antraShares/src/app/interfaces/userlist.model.ts
+++ antraShares/src/app/interfaces/userlist.model.ts
@@ -1,15 +1,12 @@
 export interface Users {
   id?: number;
-  name: string;
+  name?: string;
   userName: string;
   userEmail: string;
   password: string;
-  userRole: {
-    role: string;
-    require: boolean;
-  };
-  age: number;
-  gender: string;
-  phone: number;
-  avata: string;
+  userRole: string;
+  age?: number;
+  gender?: string;
+  phone?: number;
+  avata?: string;
 }
