f10f75200e99acad489bfe42bb6cd2352638caaf
lukeyzhong <lukeyzhong@gmail.com>
2022-03-27 16:59:03 -0500

    update

---
+++ antra_final/src/app/interface/newuser.model.ts
@@ -0,0 +1,10 @@
+export interface NewUser {
+    name?: string;
+    userName: string;
+    userEmail: string;
+    password: string;
+    userRole: string;
+    age?: number;
+    gender?: string;
+    phone?: number;
+  }
