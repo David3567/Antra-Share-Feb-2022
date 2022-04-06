e2337e9fbf71f9d07bd97b6b90de2ed0bb58e5a6
jqj5405 <35941304+jqj5405@users.noreply.github.com>
2022-04-06 16:30:12 -0400

    Implement catchError and redirect if token error

--- TeamProject/src/app/interceptors/auth.interceptor.ts
+++ TeamProject/src/app/interceptors/auth.interceptor.ts
@@ -3,23 +3,34 @@ import {
   HttpRequest,
   HttpHandler,
   HttpEvent,
-  HttpInterceptor
+  HttpInterceptor,
+  HttpErrorResponse,
 } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { catchError, Observable, of, retry, throwError } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
 
-  constructor() { }
+  constructor(private route: Router) { }
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     const token = localStorage.getItem('bearerToken');
     const reqWithAuth = request.clone({
       setHeaders: {
-        Authorization: `Bearer${token}`
+        Authorization: token ? `Bearer${token}` : ''
       }
     })
 
-    return next.handle(reqWithAuth);
+    return next.handle(reqWithAuth).pipe(
+      // retry(1),
+      catchError((err: HttpErrorResponse) => {
+        alert(err.error);
+        this.route.navigateByUrl('/');
+        return of(null);
+        // return throwError(err);
+      }
+      )
+    )
   }
 }
